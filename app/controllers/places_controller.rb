821454752340340c05af78604577804ed8b56119
Jacob Jeans <jacob.jeans@yahoo.com>
2019-06-17 17:15:34 +0000

    Pagination attempt - run it by mentor

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,7 @@
 class PlacesController < ApplicationController
   def index
-    @page = Place.paginate(page: params[:page], per_page: 2)
+    @places = Place.all
+   @posts = Place.paginate(:page => params[:page], :per_page => 5)
+
+  end
 end
