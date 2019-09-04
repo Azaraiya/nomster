f893f7327c1734f33d1b5718d3a67ebb2d1d15a4
Jacob Jeans <jacob.jeans@yahoo.com>
2019-09-04 06:35:09 +0000

    Carrrierwave issue

---
+++ db/migrate/20190904052019_add_picture_photo.rb
@@ -0,0 +1,4 @@
+class AddPicturePhoto < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_163550) do
+ActiveRecord::Schema.define(version: 2019_09_04_052019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2019_08_12_163550) do
   end
 
   create_table "photos", force: :cascade do |t|
+    t.text "caption"
+    t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
