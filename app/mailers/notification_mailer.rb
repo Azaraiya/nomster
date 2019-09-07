c786a88e00dfc3e0047cd46d1c766d07e433531c
Jacob Jeans <jacob.jeans@yahoo.com>
2019-09-07 03:59:18 +0000

    Email Basics

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,8 @@
+class NotificationMailer < ApplicationMailer
+  default from: "no-reply@nomsterapp.com" #setting up the default form
+
+  def comment_added
+    mail(to: "jacob.jeans@yahoo.com", 
+      subject: "A comment has been added to your place.")
+  end
+end
