4ae87a7487412ab50eadae1373575c12e99a2637
Jacob Jeans <jacob.jeans@yahoo.com>
2019-09-07 04:30:49 +0000

    Send email on comment

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com" #setting up the default form
 
-  def comment_added
-    mail(to: "raijinteku98@gmail.com", 
-      subject: "A comment has been added to your place.")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email, 
+      subject: "A comment has been added to #{@place.name}")
   end
 end
